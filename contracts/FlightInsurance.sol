b140512da97aa93ec9712a60db83afb8f29666ef
ignatiusoey09 <oey.ignatius@gmail.com>
2025-01-09 12:06:04 +0800

    Remove Axelar implementations

--- contracts/FlightInsurance.sol
+++ contracts/FlightInsurance.sol
@@ -1,18 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.2 <0.9;
 
-// Import IAxelarGateway from cgp library to access callContractWithToken. Aliased to cgpIAxelarGateway
-import "https://github.com/axelarnetwork/axelar-cgp-solidity/blob/main/contracts/interfaces/IAxelarGateway.sol" as cgpIAxelarGateway;
-
-
 contract FlightInsurance {
-    cgpIAxelarGateway.IAxelarGateway public cgpGateway;
     enum InsuranceTier {Basic, Enhanced}
     string public lastReceivedMessage;
 
-    constructor(address gatewayAddress) {
-        require(gatewayAddress != address(0), "Invalid address!");
-        cgpGateway = cgpIAxelarGateway.IAxelarGateway(gatewayAddress);
+    constructor(address companyWallet) {
+        require(companyWallet != address(0), "Invalid sending wallet!");
     }  
 
 
@@ -37,22 +31,29 @@ contract FlightInsurance {
     /** 
     * @dev Event triggered when transaction is successful
     */
-    event PayoutSent(string receipient, uint256 amount);
+    event PayoutSent(address receipient, uint256 amount);
+
+    /** 
+    * @dev Event triggered when transaction failed
+    */
+    event PayoutFailed(address receipient, uint256 amount, string returnData);
 
     
-    function execute( 
-        string calldata sourceChain, 
-        bytes calldata payload
+    function initiatePayout( 
+        uint8 _tier,
+        address userWallet
     ) external {
-        //payload contains: insurance tier and user XRPL wallet address
-        (uint8 tier, string memory userAddress) = abi.decode(payload, (uint8, string));
-        require(tier>=0 && tier <=1, "Invalid user tier!");
+        require(_tier>=0 && _tier <=1, "Invalid user tier!");
 
-        InsuranceTier insuranceTier = InsuranceTier(tier);
-        uint256 payout = calculatePayout(insuranceTier);
-
-        bytes memory returnPayload = ""; //Empty payload for now
-        cgpGateway.callContractWithToken(sourceChain, userAddress, returnPayload, "XRP", payout);
-        emit PayoutSent(userAddress, payout);
+        InsuranceTier insuranceTier = InsuranceTier(_tier);
+        uint16 payout = calculatePayout(insuranceTier);
+        (bool sent, bytes memory data) = payable(userWallet).call{value: payout}("");
+        if (sent) {
+            emit PayoutSent(userWallet, payout);
+        } else {
+            string memory dataString = abi.decode(data, (string));
+            emit PayoutFailed(userWallet, payout, dataString);
+        }
     }
+
 }
